2bb3212a7422194128f42e474cb35c3f27467539
master-bry <bryngowi2@gmail.com>
2026-04-16 11:37:39 +0300

    bill model

--- src/main/java/com/master/finance/model/Bill.java
+++ src/main/java/com/master/finance/model/Bill.java
@@ -35,7 +35,7 @@ public class Bill {
         this.status = "PENDING";
     }
 
-    // Getters and Setters (zote zihifadhiwe)
+    // Getters and Setters
     public String getId() { return id; }
     public void setId(String id) { this.id = id; }
     public String getUserId() { return userId; }
