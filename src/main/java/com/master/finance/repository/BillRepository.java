efed616fd3620693c59ee842f8bad7d4f9631d4a
master-bry <bryngowi2@gmail.com>
2026-04-16 11:43:55 +0300

    bill repo new

--- src/main/java/com/master/finance/repository/BillRepository.java
+++ src/main/java/com/master/finance/repository/BillRepository.java
@@ -4,13 +4,18 @@ import com.master.finance.model.Bill;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface BillRepository extends MongoRepository<Bill, String> {
+
     @Query("{ 'userId': ?0, 'deleted': false }")
     List<Bill> findByUserIdAndDeletedFalse(String userId);
 
     @Query("{ 'userId': ?0, 'status': ?1, 'deleted': false }")
     List<Bill> findByUserIdAndStatus(String userId, String status);
+
+    @Query("{ 'userId': ?0, 'deleted': false, 'dueDate': { $lte: ?1 }, 'status': 'PENDING' }")
+    List<Bill> findOverdueBills(String userId, LocalDate date);
 }
